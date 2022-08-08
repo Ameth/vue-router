a9fdf88f41b1498a6b43318f910872c3d9aa6d87
Ameth Ordoñez <amethgabriel@hotmail.com>
2022-08-08 17:06:04 -0500

    initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { ref } from "vue";
+
+const username = ref("");
+</script>
+
+<template>
+  <main>
+    <h3>Ingresa tu usuario</h3>
+    <input type="text" placeholder="Usuario" v-model="username" />
+  </main>
+</template>
